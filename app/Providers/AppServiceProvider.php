4fd3f662db08396daef70bc5304628bc231a67a3
Dariush <paeezymohammad@gmail.com>
2022-12-31 09:55:01 +0330

    bind FlightCompanyInterface to related class

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Factories\FlightCompanyFactory;
+use App\Interfaces\FlightCompanyInterface;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +15,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(FlightCompanyInterface::class, function() {
+            return FlightCompanyFactory::make(request()->company);
+        });
     }
 
     /**
